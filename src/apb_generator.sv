8b18a7897161f31e96c11bad1ec8c04ccccc70fb
Harshitha Naik <harshitharn2004@gmail.com>
2026-07-15 21:54:50 +0530

    Add APB generator class for transaction generation

---
+++ src/apb_generator.sv
@@ -0,0 +1,17 @@
+`include "define.sv"
+class apb_generator;
+apb_transaction apb_gen;
+mailbox #(apb_transaction) mb_gd;
+function new (mailbox #(apb_transaction) mb_gd);
+	this.mb_gd = mb_gd;
+	apb_gen = new();
+endfunction
+task start();
+	for(int i = 0; i < `num_trans; i++)
+	begin
+		assert(apb_gen.randomize() == 1)
+		mb_gd.put(apb_gen.copy());
+		$display("GENERATOR \t PADDR = %0d  PSEL = %b  PENABLE = %b  PWRITE = %b  PWDATA = %0d  PSTRB = %0d",apb_gen.PADDR, apb_gen.PSEL, apb_gen.PENABLE, apb_gen.PWRITE, apb_gen.PWDATA, apb_gen.PSTRB,$time);
+	end
+endtask
+endclass
